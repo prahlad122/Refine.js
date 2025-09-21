f7c20e97b8cf58b77a927dd7a20664909df1dc49
prahlad122 <prahladji1592003@gmail.com>
2025-09-21 06:02:20 +0000

    conversionOperation

---
+++ 01_test/conversion.js
@@ -0,0 +1,20 @@
+let age = "20";
+ let isLOggedIn=1;
+ let number="22abc";
+
+ let Num=Number(number);
+let AGE=Number(age);
+let LOggedIn=Boolean(isLOggedIn);
+
+console.log(age);
+console.log(typeof AGE);
+console.log(typeof LOggedIn)
+console.log(LOggedIn)
+console.log(typeof Num)
+console.log(Num)
+
+//"20" =>20
+// "34abs" => NaN this is number type
+//1 ==true
+// ""= false
+// "abdfah" => true
