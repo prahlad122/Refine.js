7cb59ceb4b1893645ff88f047868e6abe12f7bb4
prahlad122 <prahladji1592003@gmail.com>
2025-09-21 05:40:32 +0000

    variable

---
+++ 01_test/variable.js
@@ -0,0 +1,13 @@
+const accountId=32324
+let accountEmail="abcd@c.com"
+var accountPassword="12345"
+account=144525;
+let acc;
+console.table([accountId,accountEmail,accountPassword,account.acc]);
+
+
+// Use const by default.
+
+// Use let if you know the value will change.
+
+// Avoid var (old, confusing).
