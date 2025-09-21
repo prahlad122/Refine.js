eed7a9ad85aadc1638c4d5b3bd83c5b9289fd77c
prahlad122 <prahladji1592003@gmail.com>
2025-09-21 05:45:03 +0000

    Datatype

---
+++ 01_test/datatype.js
@@ -0,0 +1,14 @@
+let name="Ghaseetaram";
+let age=20;
+let address;
+
+
+//number=> 2 to power 53
+//string => ""
+//boolean => true/false
+//undefined => 
+
+    console.log(typeof age);
+    console.log("name");
+    console.log(undefined);
+    
